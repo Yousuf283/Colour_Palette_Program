7eff9833ec2f9207d6db3a6ee08bc4cdc6bf5468
yousuf-city-uni <118533199+yousuf-city-uni@users.noreply.github.com>
2024-01-29 15:22:23 +0000

    Added quit button hitbox

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 local debug = require('stats')
 local colours = require('colours')
 
+local width, height = love.graphics.getDimensions()
+
 love.load = function()
-  love.graphics.setBackgroundColor(colours.background)
+  love.graphics.setBackgroundColor(colours.primary)
 end
 
 love.update = function(dt)
@@ -15,6 +17,13 @@ love.keypressed = function(k)
   end
 end
 
+love.mousepressed = function(mx, my, button)
+  if (mx >= width-30) and (mx <= width) and (my >= 0) and (my <= 30) then
+    love.event.quit()
+  end
+end
+
 love.draw = function()
+
   debug_draw()
 end
