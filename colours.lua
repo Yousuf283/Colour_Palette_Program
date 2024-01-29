c827b4c2651d6af2ab53045deebbc094793432aa
yousuf-city-uni <118533199+yousuf-city-uni@users.noreply.github.com>
2024-01-29 15:23:09 +0000

    Added quit button hitbox

--- colours.lua
+++ colours.lua
@@ -1,5 +1,6 @@
 local colours = {}
 
-colours.background = {love.math.colorFromBytes(47, 54, 66, 255)}
+colours.primary = {love.math.colorFromBytes(47, 54, 66, 255)}
+colours.secondary = {love.math.colorFromBytes(33, 37, 43, 255)}
 
 return colours
