3e7f1d7166502ecfccd726b55089bc20c3f40ca8
yousuf-city-uni <118533199+yousuf-city-uni@users.noreply.github.com>
2024-02-01 18:03:02 +0000

    Added System Cursors

---
+++ cursor.lua
@@ -0,0 +1,8 @@
+local cursor = {}
+
+cursor.default = love.mouse.getSystemCursor('arrow')
+cursor.hand = love.mouse.getSystemCursor('hand')
+cursor.ibeam = love.mouse.getSystemCursor('ibeam')
+cursor.wait = love.mouse.getSystemCursor('wait')
+
+return cursor
